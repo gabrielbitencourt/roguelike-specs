d3ba06cebd474b2eaf366b683882c373be724068
Gabriel Bitencourt <gabrielbitencourt25@hotmail.com>
2020-10-08 23:28:17 -0300

    start rendering hero

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,11 @@
 use crate::{Player,Position,Input,Map};
-use amethyst::{GameData,SimpleState,StateData};
-use amethyst::ecs::{WorldExt,Builder};
-use amethyst::core::transform::Transform;
+use amethyst::{
+    assets::{AssetStorage, Handle, Loader},
+    core::transform::Transform,
+    ecs::{Component, DenseVecStorage},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+};
 
 #[derive(Default)]
 pub struct GameState;
@@ -17,7 +21,32 @@ impl SimpleState for GameState {
         let mut transform = Transform::default();
         transform.set_translation_xyz(25., 25., 0.);
 
+        let texture_handle = {
+            let loader = world.read_resource::<Loader>();
+            let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+            loader.load(
+                "hero.png",
+                ImageFormat::default(),
+                (),
+                &texture_storage,
+            )
+        };
+
+        let sprite_sheet_handle = {
+            let loader = world.read_resource::<Loader>();
+            let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+            loader.load(
+                "hero.ron",
+                SpriteSheetFormat(texture_handle),
+                (),
+                &sprite_sheet_store,
+            )
+        };
+        
+        let sprite_render = SpriteRender::new(sprite_sheet_handle, 0);  // paddle is the first sprite in the sprite_sheet
+
         world.create_entity()
+            .with(sprite_render)
             .with(Player::default())
             .with(Input::default())
             .with(transform)
